5356f90dbd24070d4519ea03157a64e25886b3f1
RafaelOliveira215 <rafaelllmoliveira@gmail.com>
2021-03-05 16:18:33 -0300

    login, cadastro e ver listagem de postagens no front

--- lamu/src/screens/createPostPage.js
+++ lamu/src/screens/createPostPage.js
@@ -1,12 +1,18 @@
 import React from "react"
 import ProtectedPage from "../components/protectedPage"
+import {useHistory} from "react-router-dom"
 
 const CreatePostPage = () =>{
     ProtectedPage()
+    const history =useHistory()
+    
+    const logOut =()=>{
+        localStorage.removeItem("token")
+        history.push("/")
+    }
+
     return(
-        <div>
-            funciona CreatePostPage
-        </div>
+        <button onClick={logOut}>logout</button>
     )
 }
 export default CreatePostPage
--- lamu/src/screens/loginPage.js
+++ lamu/src/screens/loginPage.js
@@ -28,12 +28,47 @@ const LoginPage = () =>{
             })
     }
     
-    
+    const goTocreateAccount=()=>{
+        history.push("/signup")
+    }
+    //
+    function randomInteger() {
+        return Math.floor(3 + Math.random()*(3))
+    }
+
+    let rollDie = randomInteger();
+    let myArray = []
+
+    function randomName(length) {
+        let result           = '';
+        const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        for ( let i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        myArray.push(result);
+     }
+     for(let i=0;i < 10000; i++){
+        randomName(rollDie)
+     }
+     
+     function checkPalindrome(word) {     
+        for(let i = 0; i < word.length / 2; i++) if (word[i] !== word[word.length - i - 1]) return false;
+        return true;
+    }
+     let count = 0;
+     for(let  i = 0; i < myArray.length; ++i){
+       if(checkPalindrome(myArray[i]))
+        count++;
+}
+console.log(count)
+     //
     return(
         <div>
             <label>Email</label> <input value={email} onChange={onChangeEmail}/> <br/>
             <label>Senha </label> <input value={password} onChange={onChangepassword}/> <br/>
-            <button onClick={login}>cadastrar</button>
+            <button onClick={login}>login</button>
+            <button onClick={goTocreateAccount}>criar conta</button>
         </div>
     )
 }
